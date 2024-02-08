a401c759760a1619f493e6ad1dd063df524385a5
Shabanaasgar1997 <shabana.webdeveloper@gmail.com>
2024-02-08 17:18:55 +0530

    removed banner

--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Fade } from "react-reveal";
 
 function Services(props) {
   const Change = () => {
@@ -48,16 +47,14 @@ function Services(props) {
         recusandae alias odio, blanditiis ea. Deleniti vel harum earum quod
         eaque maxime id consectetur.
       </p>
-      <Fade>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima,
-          maiores? Expedita, sapiente. Aut aliquam minus illum odit temporibus
-          saepe architecto quod et expedita, quam, eos possimus necessitatibus
-          excepturi nostrum? Corporis nostrum illum id vel quis accusantium
-          recusandae alias odio, blanditiis ea. Deleniti vel harum earum quod
-          eaque maxime id consectetur.
-        </p>
-      </Fade>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima,
+        maiores? Expedita, sapiente. Aut aliquam minus illum odit temporibus
+        saepe architecto quod et expedita, quam, eos possimus necessitatibus
+        excepturi nostrum? Corporis nostrum illum id vel quis accusantium
+        recusandae alias odio, blanditiis ea. Deleniti vel harum earum quod
+        eaque maxime id consectetur.
+      </p>
     </div>
   );
 }
